7751b5d255547fc304b35f82a399903b81def5e9
Thin Creator 3483 <102002463+SaxbyMod@users.noreply.github.com>
2023-12-19 16:03:54 -0500

    Delete obj directory

--- obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ExampleMod
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "ATribalLibary";
-        public const string PLUGIN_NAME = "ATribalLibary";
-        public const string PLUGIN_VERSION = "1.14.0";
-    }
-}
